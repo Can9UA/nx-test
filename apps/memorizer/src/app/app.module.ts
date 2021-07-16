fbbfa8dcd04db31da7657e570ec00ed6319d0683
Alexandr <opidd@softserveinc.com>
2021-07-16 19:43:34 +0300

    remove test-component

--- apps/memorizer/src/app/app.module.ts
+++ apps/memorizer/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LibsMaterialModule } from "@material";
-import { TestComponentComponent } from './components/test-component/test-component.component';
 
 @NgModule({
-  declarations: [AppComponent, TestComponentComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, BrowserAnimationsModule, LibsMaterialModule],
   providers: [],
   bootstrap: [AppComponent],
