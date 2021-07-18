5d5b02f1b26cf78cc9592600c6d018f6dab07865
Alexandr <opidd@softserveinc.com>
2021-07-18 13:32:42 +0300

    add Accessibility addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: [],
   addons: [
     '@storybook/addon-essentials',
-    '@storybook/addon-viewport'
+    '@storybook/addon-viewport',
+    '@storybook/addon-a11y',
   ],
 };
